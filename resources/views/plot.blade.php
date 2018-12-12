3ea163bbe3fb88cf5ccc8260bd8a44776ce38ed2
rbany <302157g@gmail.com>
2018-12-12 20:07:03 +0100

    zmiany

--- resources/views/plot.blade.php
+++ resources/views/plot.blade.php
@@ -5,7 +5,7 @@
     <canvas id="line-chart" width="5" height="5"></canvas>
     <style>
         #line-chart {
-            width:85% !important;
+            width:65% !important;
             height:50% !important;
         }
     </style>
@@ -14,11 +14,21 @@
             var o = Math.round, r = Math.random, s = 255;
             return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
         }
+        function getRandomRolor() {
+            var letters = '012345'.split('');
+            var color = '#';        
+            color += letters[Math.round(Math.random() * 5)];
+            letters = '0123456789ABCDEF'.split('');
+            for (var i = 0; i < 5; i++) {
+                color += letters[Math.round(Math.random() * 15)];
+            }
+            return color;
+        }
         var labels = {!! json_encode($labels) !!};
         var data = {!! json_encode($data) !!};
         for(var i in data) {
             data[i]['fill'] = false;
-            data[i]['borderColor'] = random_rgba();
+            data[i]['borderColor'] = getRandomRolor();
         }
         new Chart(document.getElementById("line-chart"), {
             type: 'line',
