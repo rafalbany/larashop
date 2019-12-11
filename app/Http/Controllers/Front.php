2e207426f620ae3229307a692ac14d7fde3a4589
dordomian <dordomian@op.pl>
2019-12-11 20:38:56 +0100

    kazdy z zapisow dziala

--- app/Http/Controllers/Front.php
+++ app/Http/Controllers/Front.php
@@ -57,6 +57,60 @@ class Front extends Controller {
         return view('home', array('title' => 'Welcome','description' => '','page' => 'home', 'brands' => $this->brands, 'categories' => $this->categories, 'products' => $this->products));
     }
 
+    /*
+* This function searchs in LDAP tree ($ad -LDAP link identifier)
+* entry specified by samaccountname and returns its DN or epmty
+* string on failure.
+*/
+    function getDN($ad, $samaccountname, $basedn) {
+        $attributes = array('dn');
+        $result = ldap_search($ad, $basedn,
+            "(samaccountname={$samaccountname})", $attributes);
+        if ($result === FALSE) { return ''; }
+        $entries = ldap_get_entries($ad, $result);
+        if ($entries['count']>0) { return $entries[0]['dn']; }
+        else { return ''; };
+    }
+
+    /*
+    * This function retrieves and returns CN from given DN
+    */
+    function getCN($dn) {
+        preg_match('/[^,]*/', $dn, $matchs, PREG_OFFSET_CAPTURE, 3);
+        return $matchs[0][0];
+    }
+
+    /*
+    * This function checks group membership of the user, searching only
+    * in specified group (not recursively).
+    */
+    function checkGroup($ad, $userdn, $groupdn) {
+        $attributes = array('members');
+        $result = ldap_read($ad, $userdn, "(memberof={$groupdn})", $attributes);
+        if ($result === FALSE) { return FALSE; };
+        $entries = ldap_get_entries($ad, $result);
+        return ($entries['count'] > 0);
+    }
+
+    /*
+    * This function checks group membership of the user, searching
+    * in specified group and groups which is its members (recursively).
+    */
+    function checkGroupEx($ad, $userdn, $groupdn) {
+        $attributes = array('memberof');
+        $result = ldap_read($ad, $userdn, '(objectclass=*)', $attributes);
+        if ($result === FALSE) { return FALSE; };
+        $entries = ldap_get_entries($ad, $result);
+        if ($entries['count'] <= 0) { return FALSE; };
+        if (empty($entries[0]['memberof'])) { return FALSE; } else {
+            for ($i = 0; $i < $entries[0]['memberof']['count']; $i++) {
+                if ($entries[0]['memberof'][$i] == $groupdn) { return TRUE; }
+                elseif (checkGroupEx($ad, $entries[0]['memberof'][$i], $groupdn)) { return TRUE; };
+            };
+        };
+        return FALSE;
+    }
+
     public static function auth($user, $password) {
         if(empty($user) || empty($password)) return false;
 
@@ -66,8 +120,11 @@ class Front extends Controller {
         $username = 'mary.smith';
         $password = 'Pass@word1!';
 
-        $ldaprdn = 'mycompany' . "\\" . $username;
-        $ldaprdn = 'CN=Mary Smith,CN=Users,DC=mycompany,DC=local';
+        $ldaprdn = 'mycompany' . "\\" . $username;                 //działa
+        $ldaprdn = 'CN=Mary Smith,CN=Users,DC=mycompany,DC=local'; //działa
+
+        $domain = 'mycompany.local';
+        $ldaprdn = "{$username}@{$domain}";                        //działa
 
         ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
         ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
